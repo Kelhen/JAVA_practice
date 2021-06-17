77fc8219c9a858e8877e2d91c6da30f71c9b5520
erika-goodwin <shellvoerika@gmail.com>
2021-06-17 12:27:16 -0700

    20210617

---
+++ 20210616_WMAD202/src/com/Main.java
@@ -0,0 +1,21 @@
+package com;
+
+import com.phone.GooglePixel;
+import com.phone.Iphone12;
+
+public class Main {
+    public static void main(String[] args) {
+//        GooglePixel gp = new GooglePixel();
+//        System.out.println(gp.procesor());
+//
+//        Iphone12 ip12 = new Iphone12();
+//        System.out.println(ip12.procesor());
+
+        //POLYMORHISM VERSION
+        Phone gp = new GooglePixel();
+        System.out.println(gp.procesor());
+
+        Phone ip12 = new Iphone12();
+        System.out.println(ip12.procesor());
+    }
+}
