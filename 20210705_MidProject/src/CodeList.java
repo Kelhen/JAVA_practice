f3a6587563bde515c7991ae7187975ad2597ca94
erika-goodwin <shellvoerika@gmail.com>
2021-07-06 18:30:02 -0700

    20210706

--- 20210705_MidProject/src/CodeList.java
+++ 20210705_MidProject/src/CodeList.java
@@ -1,58 +1,99 @@
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class CodeList {
-
-    void showMenu() {
-
-        char option;
-        Scanner scanner = new Scanner(System.in);
-
-        do {
-            System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
-            System.out.println("Enter Your Option");
-            System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
-            System.out.println("A : Review the code list");
-            System.out.println("B : Add new code");
-            System.out.println("C : Practice your code");
-            System.out.println("D : End of the practice");
-            option = scanner.next().charAt(0);
-            System.out.println("\n");
-
-
-            switch (Character.toLowerCase(option)){
-                case 'a':
-                    System.out.println("-------------------------------------------------------");
-                    System.out.println("Remember the code below");
-                    System.out.println("-------------------------------------------------------");
-                    System.out.println();
-                    System.out.println("\n");
-                    break;
-                case 'b':
-                    System.out.println("-------------------------------------------------------");
-                    System.out.println("Type the name of the vegitable you would like to add: ");
-                    System.out.println("-------------------------------------------------------");
-                    System.out.println("\n");
-                    break;
-                case 'c':
-                    System.out.println("-------------------------------------------------------");
-                    System.out.println("Type the code for: ");
-                    System.out.println("-------------------------------------------------------");
-                    break;
-
-                default:
-                    System.out.println("Invalid Option!! Please Enter Correct Opton...");
-                    break;
-            }
-
-        } while (option != 'D');
-        System.out.println("Good job!! See you tomorrow!");
-        scanner.close();
-
-    }
-//    Array array = new Array();
-
-}
-
-
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//public class CodeList {
+//
+//    Scanner scanner = new Scanner(System.in);
+//
+//
+//    ArrayList<ListOfCode> list = new ArrayList<>();
+//    ListOfCode banana = new ListOfCode("Banana",4011);
+//        list.add(banana);
+//
+//    ListOfCode galaApple = new ListOfCode("Gala Apple",4171);
+//        list.add(galaApple);
+//
+//    ListOfCode lemon = new ListOfCode("Lemon",4053);
+//        list.add(lemon);
+//
+//    ListOfCode rime = new ListOfCode("Rime",4048);
+//        list.add(rime);
+//
+//    ListOfCode cherry = new ListOfCode("Cherry",4259);
+//        list.add(cherry);
+//
+//
+//    void showMenu() {
+//        char option;
+//
+//
+//        do {
+//            System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+//            System.out.println("Enter Your Option");
+//            System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+//            System.out.println("A : Review the code list");
+//            System.out.println("B : Add new code");
+//            System.out.println("C : Practice your code");
+//            System.out.println("D : End of the practice");
+//            option = scanner.next().charAt(0);
+//            System.out.println("\n");
+//
+//
+//            switch (Character.toLowerCase(option)){
+//                case 'a':
+//                    System.out.println("-------------------------------------------------------");
+//                    System.out.println("Remember the code below");
+//                    System.out.println("-------------------------------------------------------");
+//                    displayProduct(list);
+//                    System.out.println("\n");
+//                    break;
+//                case 'b':
+//                    System.out.println("-------------------------------------------------------");
+//                    System.out.println("Type the name of the vegitable you would like to add: ");
+//                    System.out.println("-------------------------------------------------------");
+//                    System.out.println("\n");
+//                    break;
+//                case 'c':
+//                    System.out.println("-------------------------------------------------------");
+//                    System.out.println("Type the code for: ");
+//                    System.out.println("-------------------------------------------------------");
+//                    break;
+//
+//                default:
+//                    System.out.println("Invalid Option!! Please Enter Correct Opton...");
+//                    break;
+//            }
+//
+//        } while (option != 'D');
+//        System.out.println("Good job!! See you tomorrow!");
+//        scanner.close();
+//
+//    }
+//
+//    static void displayProduct(ArrayList<ListOfCode> list) {
+//        for(ListOfCode nameAndCode: list){
+//            System.out.println( nameAndCode.getcode()+ ": " + nameAndCode.getName());
+//        }
+//    }
+//
+//
+//    public static class ListOfCode {
+//        private String name;
+//        private int code;
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public int getcode() {
+//            return code;
+//        }
+//
+//        public ListOfCode(String name, int number) {
+//            this.name = name;
+//            this.code = number;
+//        }
+//    }
+//
+//}
+//
--- 20210705_MidProject/src/Main.java
+++ 20210705_MidProject/src/Main.java
@@ -1,15 +1,31 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+//***************************************************************************
 public class Main {
     public static void main(String[] args) {
         CodeList codeList = new CodeList();
 
-//        codeList.showMenu();
+        codeList.showMenu();
+        CodeList.displayProduct(codeList.list);
+    }
+}
 
 
+//***************************************************************************
+class CodeList {
 
-        ArrayList<ListOfCode> list = new ArrayList<>();
+
+//--------------------------------------------------------
+//-------------------ARRAY LIST --------------------------
+//--------------------------------------------------------
+
+    ArrayList<ListOfCode> list = new ArrayList<>();
+
+    //constructor
+    public void Strage(){
+
         ListOfCode banana = new ListOfCode("Banana",4011);
         list.add(banana);
 
@@ -24,9 +40,6 @@ public class Main {
 
         ListOfCode cherry = new ListOfCode("Cherry",4259);
         list.add(cherry);
-
-        displayProduct(list);
-
     }
 
     static void displayProduct(ArrayList<ListOfCode> list) {
@@ -35,24 +48,114 @@ public class Main {
         }
     }
 
+    class ListOfCode {
+        private String name;
+        private int code;
 
-    public static class ListOfCode {
-    private String name;
-    private int code;
+        public String getName() {
+            return name;
+        }
 
-    public String getName() {
-        return name;
+        public int getcode() {
+            return code;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public ListOfCode(String name, int number) {
+            this.name = name;
+            this.code = number;
+        }
     }
 
-    public int getcode() {
-        return code;
+    void addProduct(){
+        String addingName =  scanner.next();
+        Integer addingCode = scanner.nextInt();
+        ListOfCode input = new ListOfCode(addingName, addingCode);
+        list.add(input);
     }
 
-    public ListOfCode(String name, int number) {
-        this.name = name;
-        this.code = number;
+    void practiceMode(){
+        CodeList obj = new CodeList();
+        var index = obj.getRandamElement(list);
+
+
     }
+
+    private ListOfCode getRandamElement(ArrayList<ListOfCode> list) {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+
+
+    //--------------------------------------------------------
+//------------------- SHOW MENU --------------------------
+//--------------------------------------------------------
+    Scanner scanner = new Scanner(System.in);
+
+    void showMenu() {
+        char option;
+
+
+        do {
+            System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+            System.out.println("Enter Your Option");
+            System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+            System.out.println("A : Review the code list");
+            System.out.println("B : Add new code");
+            System.out.println("C : Practice your code");
+            System.out.println("D : End of the practice");
+            option = scanner.next().charAt(0);
+            System.out.println("\n");
+
+
+            switch (Character.toLowerCase(option)){
+                case 'a':
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("Remember the code below");
+                    System.out.println("-------------------------------------------------------");
+                    displayProduct(list);
+                    System.out.println("\n");
+                    break;
+                case 'b':
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("Type the name of the vegitable you would like to add: ");
+                    System.out.println("First: the NAME of the vegitable/fruit");
+                    System.out.println("Second: the CODE of the vegitable/fruit");
+                    System.out.println("-------------------------------------------------------");
+                    addProduct();
+                    System.out.println("\n");
+                    break;
+                case 'c':
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("Type the code for: " + list.get());
+
+
+                    option1 = scanner.next().charAt(0);
+
+                    System.out.println("-------------------------------------------------------");
+                    break;
+
+                default:
+                    System.out.println("Invalid Option!! Please Enter Correct Opton...");
+                    break;
+            }
+
+        } while (option != 'D');
+        System.out.println("Good job!! See you tomorrow!");
+        scanner.close();
+
+    }
+
+
+
 }
 
 
-}
+
